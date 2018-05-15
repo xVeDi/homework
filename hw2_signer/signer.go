a3d0631fc57ccb2dcc650e2d76202a711fb12b9c
Dmitry Vecherov <xvedi@yandex.ru>
2018-05-16 00:05:08 +0300

    20180515

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"sort"
 	"strconv"
+	"strings"
 )
 
-func SingleHash(data string) string {
+func SingleHash(data string) (a string) {
 	md := DataSignerMd5(data)
-	return DataSignerCrc32(data) + "~" + DataSignerCrc32(md)
+	a = DataSignerCrc32(data) + "~" + DataSignerCrc32(md)
+	println(a)
+	return
 }
 
 func MultiHash(data string) string {
@@ -20,14 +25,90 @@ func MultiHash(data string) string {
 		// fmt.Println(data, " MultiHash: crc32(th+step1))", i, " ", maps[i])
 	}
 
-	return func(mps []string) (a string) {
-		for _, v := range mps {
+	return func(maps []string) (a string) {
+		for _, v := range maps {
 			a += v
 		}
 		return
 	}(maps)
 }
 
+func CombineResults(in []string) (a string) {
+
+	sort.Strings(in)
+
+	for _, v := range in {
+		a = a + "_" + v
+
+	}
+	return strings.TrimLeft(a, "_")
+}
+
+func ExecutePipeline(jbs ...job) {
+
+	workerInput := make(chan interface{}, len(jbs))
+	for _, jb := range jbs {
+		//	workerInput <- jb(nil, workerInput)
+		//fmt.Printf("%T\n", jbs[0])
+		go jb(workerInput, workerInput)
+	}
+	go worker(workerInput)
+
+}
+
+func worker(in <-chan interface{}) {
+	for {
+		//	time.Sleep(time.Second)
+		// fmt.Println("There is worker...I recieved  value <--", <-val)
+
+		for i := range in {
+
+			ii, ok := i.(int)
+
+			if !ok {
+				fmt.Println("Не удалось преобразовать к типу *int")
+			}
+
+			st := strconv.Itoa(ii)
+			go SingleHash(st)
+
+			//			fmt.Printf("%T\n", i)
+		}
+	}
+
+}
+
 func main() {
-	println(MultiHash(SingleHash("0")))
+
+	inputData := []int{0, 1}
+
+	hashSignJobs := []job{
+		job(func(in, out chan interface{}) {
+			for _, fibNum := range inputData {
+				out <- fibNum
+			}
+		}),
+		// job(func(in, out chan interface{}) {
+		// 	for i := 50; i < 55; i++ {
+		// 		out <- i
+		// 	}
+		// }),
+	}
+
+	ExecutePipeline(hashSignJobs...)
+
+	fmt.Scanln()
+	fmt.Println("done")
+
+	// res := []string{"4958044192186797981418233587017209679042592862002427381542", "29568666068035183841425683795340791879727309630931025356555"}
+	// res := []string{}
+	// res = append(res, MultiHash(SingleHash("1")))
+	// res = append(res, MultiHash(SingleHash("0")))
+	// res = append(res, MultiHash(SingleHash("1")))
+	// res = append(res, MultiHash(SingleHash("2")))
+	// res = append(res, MultiHash(SingleHash("3")))
+	// res = append(res, MultiHash(SingleHash("5")))
+	// res = append(res, MultiHash(SingleHash("8")))
+
+	// fmt.Println(CombineResults(res))
 }
